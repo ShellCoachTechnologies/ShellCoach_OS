6b98b05ee6a28ce053e83d58344ce3dfbf265da6
ShellCoachTechnologies <ShellCoachTechnologies@gmail.com>
2025-06-18 23:34:23 -0400

    Delete server/app.py

--- server/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/hello')
-def hello():
-    return jsonify(message='Hello from Flask backend!')
